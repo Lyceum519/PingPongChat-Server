1360c9e675a2e8e850a34c5706dfe1655d5cf099
UnoRich <wonyeong91@gmail.com>
2018-11-11 16:53:51 +0900

    feat: build Record API
    Signed-off-by: UnoRich <wonyeong91@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import path from 'path';
 import User from '../model/user';
 import dbConnection from '../util/db_con';
-import { findAllUser } from '../util/sequelize/api/find'
+import { findAllUser, findAllRecord, findRecordByUsers } from '../util/sequelize/api/find'
 
 let router = express.Router();
 const database = dbConnection().init();
@@ -16,29 +17,46 @@ let welcome = [
 	{
     pong: "visit"
 	},
-	{
+  {
     chat: "us!"
 	}
 ];
 res.json(welcome);
 });
 
-router.get('/users', async (req, res, next ) => {
-let users = [
-	new User('James Coonce', 'none@none.com'),
-	new User('Bob Coonce', 'none@none.com'),
-	new User('Euri', 'none@none.com'),
-	new User('Norman','none@none.com'),
-];
-const dbUsers = await findAllUser().then((res) => {
-	return res;
-});
-res.json(dbUsers);
+
+/* Get all of users */
+router.get('/users', async (req, res, next) => {
+  const dbUsers = await findAllUser().then((res) => {
+  	return res;
+  });
+  res.json(dbUsers);
 });
 
+/* Create user */
 router.post('/user/create', (req, res) => {
   let user = new User(req.body.name, req.body.email);
   res.json(user);
+})
+
+/* Get all of records */
+router.get('/records', async(req, res, next) => {
+	const dbRecords = await findAllRecord().then((res) => {
+    return res;
+  })
+  res.json(dbRecords);
+})
+
+/* Get record with parmeter
+   ex) /records/who?from=wonyeong&to=minwoo
+*/
+router.get('/records/who', async(req, res, next) => {
+  const dbRecord = await findRecordByUsers(req.query.from, req.query.to)
+  .then((res) => {
+    return res;
+  })
+  res.sendFile(path.join(__dirname, '../', dbRecord.dataValues.record));
+  // res.json(dbRecord);
 })
 
 export default router;
--- util/sequelize/api/find.js
+++ util/sequelize/api/find.js
@@ -1,9 +1,25 @@
 const User = require('../model/user');
+const Record = require('../model/record');
 
 export function findAllUser() {
   return User.findAll().then((users) => {
-    console.log(users);
     return users;
   });
 }
 
+export function findAllRecord() {
+  return Record.findAll().then((records) => {
+    return records;
+  })
+}
+
+export function findRecordByUsers(from, to) {
+  return Record.findOne({
+    where: {
+      from: from,
+      to: to,
+    }
+  }).then((records) => {
+    return records;
+  })
+}
