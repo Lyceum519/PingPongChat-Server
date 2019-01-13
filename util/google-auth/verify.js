ef467e0cde855c49c8fca02f21b3308de0be817f
UnoRich <wonyeong91@gmail.com>
2019-01-13 17:42:31 +0900

    feat: verify google signin token
    Signed-off-by: UnoRich <wonyeong91@gmail.com>

---
+++ util/google-auth/verify.js
@@ -0,0 +1,17 @@
+import {OAuth2Client} from 'google-auth-library';
+const CLIENT_ID = '54618363609-juc0jd4ckgct0ndmfp1emritcu38bliv.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
+export default async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID,
+    // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+}
+
